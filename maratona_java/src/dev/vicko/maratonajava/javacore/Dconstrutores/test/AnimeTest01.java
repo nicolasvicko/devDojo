ee353bd17827011f3e6f22305215cd01335f4068
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-01 17:40:49 -0300

    59 - Orientação Objetos - Construtores pt 02 - Sobrecarga

--- maratona_java/src/dev/vicko/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -6,6 +6,7 @@ public class AnimeTest01 {
 
     public static void main(String[] args) {
 
+        //Criando objeto com parÂmetros.
         Anime anime = new Anime("Akudama Drive", "TV", 13, "Ação");
         anime.imprime();
 
