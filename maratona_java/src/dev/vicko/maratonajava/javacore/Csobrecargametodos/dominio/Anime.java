205a7be702291b76e16c84b85edcdad2d1c8122e
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-01 15:59:40 -0300

    57 - Orientação Objetos - Sobrecarga de métodos

--- maratona_java/src/dev/vicko/maratonajava/javacore/Csobrecargametodos/dominio/Anime.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Csobrecargametodos/dominio/Anime.java
@@ -1,8 +1,46 @@
 package dev.vicko.maratonajava.javacore.Csobrecargametodos.dominio;
 
 public class Anime {
+    private String nome;
     private String tipo;
-    private int eposidios;
+    private int episodios;
+    private String genero;
+
+    public Anime(String nome, String tipo, int episodios) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    public void  init (String name, String tipo, int episodios){
+        this.nome = name;
+        this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    public void  init (String name, String tipo, int episodios, String genero){
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
+
+    public Anime() {
+
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public String getTipo() {
         return tipo;
@@ -12,11 +50,19 @@ public class Anime {
         this.tipo = tipo;
     }
 
-    public int getEposidios() {
-        return eposidios;
+    public int getEpisodios() {
+        return episodios;
     }
 
-    public void setEposidios(int eposidios) {
-        this.eposidios = eposidios;
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
 }
--- maratona_java/src/dev/vicko/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -1,10 +1,16 @@
 package dev.vicko.maratonajava.javacore.Csobrecargametodos.test;
 
+import dev.vicko.maratonajava.javacore.Csobrecargametodos.dominio.Anime;
+
 public class AnimeTest01 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello monster kill god!!!     ");
+        Anime anime = new Anime();
+        anime.init("Akudama Drive", "TV", 12, "Ação");
+        anime.init("Akudama Drive 2", "TV", 12);
+
+        anime.imprime();
 
     }
 }
