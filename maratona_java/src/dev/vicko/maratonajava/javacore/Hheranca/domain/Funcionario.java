586fea673d9553e095b84ecb1055b641e5ac12f1
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-02 23:20:38 -0300

    74 - Orientação Objetos - Herança pt 04 - Construtores

--- maratona_java/src/dev/vicko/maratonajava/javacore/Hheranca/domain/Funcionario.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Hheranca/domain/Funcionario.java
@@ -3,6 +3,15 @@ package dev.vicko.maratonajava.javacore.Hheranca.domain;
 public class Funcionario extends Pessoa {
 	 private double salario;
 
+
+	 /*
+	 Ao adicionar construtor na classe mãe, obrigatoriamente precisamos implementar esse construtor em todas as classes filhas.
+	 Chamar o super() sem argumentos não precisa. essa chamada é implícita.
+	  */
+	 public Funcionario (String nome) {
+		  super(nome);
+	 }
+
 	 public void  imprime(){
 		  super.imprime();
 		  System.out.println(this.salario);
