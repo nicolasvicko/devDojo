c0b7e2c47a34bc25e05a8b709bae50bc094ecab5
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-04 15:11:43 -0300

    77 - Orientação Objetos - Modificador final pt 01 - Tipo primitivo

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Jmodificadorfinal/domain/Carro.java
@@ -0,0 +1,14 @@
+package dev.vicko.maratonajava.javacore.Jmodificadorfinal.domain;
+
+public class Carro {
+	 private String nome;
+	 public static final double VELOCIDADE_LIMITE = 250;
+
+	 public String getNome () {
+		  return nome;
+	 }
+
+	 public void setNome (String nome) {
+		  this.nome = nome;
+	 }
+}
---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package dev.vicko.maratonajava.javacore.Jmodificadorfinal.test;
+
+import dev.vicko.maratonajava.javacore.Jmodificadorfinal.domain.Carro;
+
+public class CarroTest01 {
+	 public static void main (String[] args) {
+		  Carro carro = new Carro();
+		  System.out.println(Carro.VELOCIDADE_LIMITE);
+	 }
+}
