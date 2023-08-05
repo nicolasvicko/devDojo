ceb2066ac316ae43d4cebca66c499aae90252eaf
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-05 18:57:29 -0300

    84 - Orientação Objetos - Classes abstratas pt 01

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Lclassesabstratas/domain/Funcionario.java
@@ -0,0 +1,19 @@
+package dev.vicko.maratonajava.javacore.Lclassesabstratas.domain;
+
+/*
+Classes podem ser abstradas e sua finalidade é ser extendida.
+Não pode usar ou construr qualquer objeto da classe abstrata( 'new()')
+
+Final proibe a implementação não podendo extender as funcionaliades da classe.
+abstract obriga a implementação
+ */
+public abstract class Funcionario {
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+}
