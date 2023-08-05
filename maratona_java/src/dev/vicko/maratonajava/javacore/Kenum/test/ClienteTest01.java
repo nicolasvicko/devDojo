ca3dbf94aca3fa35b084a210003b2b84e444fd74
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-05 15:14:53 -0300

    81 - Orientação Objetos - Enumeração pt 02 - Construtores e atributos

--- maratona_java/src/dev/vicko/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -1,20 +1,20 @@
 package dev.vicko.maratonajava.javacore.Kenum.test;
 
 import dev.vicko.maratonajava.javacore.Kenum.domain.Cliente;
+import dev.vicko.maratonajava.javacore.Kenum.domain.Cliente.TipoPagamento;
 import dev.vicko.maratonajava.javacore.Kenum.domain.TipoCliente;
 
-public class ClienteTest01 {
-	 public static void main (String[] args) {
-		  Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-		  Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-		  Cliente cliente3 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-		  Cliente cliente4 = new Cliente("Tsubasa", TipoCliente.PESSOAJURIDICA);
+import static dev.vicko.maratonajava.javacore.Kenum.domain.Cliente.TipoPagamento.CREDITO;
+import static dev.vicko.maratonajava.javacore.Kenum.domain.Cliente.TipoPagamento.DEBITO;
 
-		  System.out.println(cliente1);
-		  System.out.println(cliente2);
-		  System.out.println(cliente3);
-		  System.out.println(cliente4);
-		  //commit test 2
-		 //commit test 3
-	 }
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, CREDITO);
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+
+
+    }
 }
