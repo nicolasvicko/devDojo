4270cd6f4588ef203612e37a452507951d0cd34b
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-07 18:48:02 -0300

    101 - Exceções pt 07 - Capturando múltiplas exceções

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,38 @@
+package dev.vicko.maratonajava.javacore.Oexception.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new ArrayIndexOutOfBoundsException();
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Dentro do IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("Dentro do ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+
+        try {
+            talvesLanceException();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    private static void talvesLanceException () throws SQLException, FileNotFoundException {
+    }
+
+}
