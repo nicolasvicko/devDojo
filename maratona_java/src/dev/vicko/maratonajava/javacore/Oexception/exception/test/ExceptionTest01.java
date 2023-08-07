0f04712cc906c27a96e2d9bb6103a147203213b7
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-07 17:26:01 -0300

    97 - Exceções pt 03 - Exception

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Oexception/exception/test/ExceptionTest01.java
@@ -0,0 +1,23 @@
+package dev.vicko.maratonajava.javacore.Oexception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+
+    }
+
+    private static void criarNovoArquivo() {
+        File file = new File("Arquivo/teste01.txt");
+        try {
+           boolean isCriado=  file.createNewFile();
+            System.out.println("Arquivo "+isCriado + " criado.");
+        } catch (IOException e) {
+            System.out.println("deu erro");
+            e.printStackTrace();
+
+        }
+    }
+}
