3bf89c6aeff38795c62597a4ada67250b97d2125
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-07 21:11:40 -0300

    Começando com Wrappers

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Pwrappers/test/WrapperTest01.java
@@ -0,0 +1,22 @@
+package dev.vicko.maratonajava.javacore.Pwrappers.test;
+
+public class WrapperTest01 {
+    byte byteP = 1;
+    short shorpP = 1;
+    int intP = 1;
+    long longP = 10L;
+    float floatP = 10F;
+    double doubleP = 10D;
+    char charP = "W";
+    boolean booleanP = false;
+
+    Byte byteW = 1;
+    Short shorWW = 1;
+    Integer intW = 1;
+    Long longW = 10L;
+    Float floatW = 10F;
+    Double doubleW = 10D;
+    Character charW = "W";
+    Boolean booleanW = false;
+
+}
