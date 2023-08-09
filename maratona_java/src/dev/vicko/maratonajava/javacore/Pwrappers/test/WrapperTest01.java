ccb3db3e73425af3bbafdc195925274713b86965
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-09 10:52:11 -0300

    106 - Classes Utilitárias - Wrappers pt 01

--- maratona_java/src/dev/vicko/maratonajava/javacore/Pwrappers/test/WrapperTest01.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Pwrappers/test/WrapperTest01.java
@@ -7,16 +7,21 @@ public class WrapperTest01 {
     long longP = 10L;
     float floatP = 10F;
     double doubleP = 10D;
-    char charP = "W";
+    char charP = "W";vv
     boolean booleanP = false;
 
+    /*
+    Em collections e outros tópicos avançados em java não se usa mais tipos primitivos.
+    Agora os valores são tipo referência e são objetos.
+    Só usar tipo Wrappers quando for necessário. e sempre que possível o tipo primitivo.
+     */
     Byte byteW = 1;
     Short shorWW = 1;
     Integer intW = 1;
     Long longW = 10L;
     Float floatW = 10F;
     Double doubleW = 10D;
-    Character charW = "W";
+    Character charW = 'W';
     Boolean booleanW = false;
 
 }
