8528b8a95f55ca1e1931db2040d9b50b3125b31b
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-09 20:17:45 -0300

    109 - Classes Utilitárias - Strings pt 02

--- maratona_java/src/dev/vicko/maratonajava/javacore/Qstring/test/StringTest01.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -4,10 +4,12 @@ public class StringTest01 {
     public static void main(String[] args) {
         String nome = "William"; //String constant pool
         String nome2 = "William";
-        nome.concat("Suane");
-        System.out.println(nome == nome2);
+        String nomeConcat = "William";
+        nomeConcat = nomeConcat.concat(" Suane"); //nome +="Suane"
+        System.out.println(nomeConcat);
+        System.out.println(nome == nome2); // é pra comparar referências, e não o valor em si do objeto
 
-        String nome3 = new String("William"); // criando uma variável de referência, objeto do tipo string, 3 uma string no pool de strings.
+        String nome3 = new String("William"); //1 criando uma variável de referência, 2 objeto do tipo string, 3 uma string no pool de strings.
         System.out.println(nome2 == nome3);
         System.out.println(nome2 == nome3.intern());
 
