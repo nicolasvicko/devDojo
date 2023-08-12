65b288ba93b7b792262f713849362f8e78a9fbcb
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-12 14:30:58 -0300

    115 - Classes Utilitárias - Internacionalização Datas com Locale

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Rdates/test/LocaleTest01.java
@@ -0,0 +1,38 @@
+package dev.vicko.maratonajava.javacore.Rdates.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+	 public static void main (String[] args) {
+		  Locale localeItaly = new Locale("IT", "IT");
+		  Locale localeCH = new Locale("IT", "CH");
+		  Locale localeIndia = new Locale("hi", "IN");
+		  Locale localeJapao = new Locale("ja", "jp");
+		  Locale localeHolanda = new Locale("nl", "NL");
+
+		  Calendar calendar = Calendar.getInstance();
+
+		  DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+		  DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeCH);
+		  DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeIndia);
+		  DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL, localeJapao);
+		  DateFormat df5 = DateFormat.getDateInstance(DateFormat.FULL, localeHolanda);
+
+		  System.out.println("Itália " + df1.format(calendar.getTime()));
+		  System.out.println("----------");
+		  System.out.println("Suiça " + df2.format(calendar.getTime()));
+		  System.out.println("----------");
+		  System.out.println("India " + df3.format(calendar.getTime()));
+		  System.out.println("----------");
+		  System.out.println("Japão " + df4.format(calendar.getTime()));
+		  System.out.println("----------");
+		  System.out.println("Holanda " + df5.format(calendar.getTime()));
+
+
+		  System.out.println(localeItaly.getDisplayCountry(localeJapao));
+		  System.out.println(localeCH.getDisplayCountry(localeHolanda));
+		  System.out.println(localeItaly.getDisplayLanguage(localeJapao));
+	 }
+}
