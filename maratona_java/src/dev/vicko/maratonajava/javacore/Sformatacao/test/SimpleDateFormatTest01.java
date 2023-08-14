5f0cf2167123338d936a70fbc3eded99a4aa8316
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-14 14:39:00 -0300

    118 - Classes Utilitárias - SimpleDateFormat

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Sformatacao/test/SimpleDateFormatTest01.java
@@ -0,0 +1,19 @@
+package dev.vicko.maratonajava.javacore.Sformatacao.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatTest01 {
+	 public static void main (String[] args) {
+		  String pattern = " 'Amsterdan' dd 'de' MMMM 'de' yyyy";
+		  SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		  System.out.println(sdf.format(new Date()));
+
+		  try {
+				System.out.println(sdf.parse(" Amsterdan 14 de agosto de 2023"));
+		  } catch (ParseException e) {
+				e.printStackTrace();
+		  }
+	 }
+}
