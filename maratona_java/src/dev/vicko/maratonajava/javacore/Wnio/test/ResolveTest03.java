8bc123b6926995b88d11e0773640d3b548f5c0ef
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-21 11:08:20 -0300

    148 - Classes Utilitárias - NIO pt 05 - Relativize

--- maratona_java/src/dev/vicko/maratonajava/javacore/Wnio/test/ResolveTest03.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Wnio/test/ResolveTest03.java
@@ -3,7 +3,7 @@ package dev.vicko.maratonajava.javacore.Wnio.test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PathTest03 {
+public class ResolveTest03 {
 	 public static void main (String[] args) {
 
 		  Path dir = Paths.get("home/nicol");
