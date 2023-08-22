9e14eb6c894eae2ee87067a2b2b39660b5bebb0c
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-22 14:31:04 -0300

    158 - Classes Utilitárias - NIO pt 15 - ZipOutputStream

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
@@ -0,0 +1,34 @@
+package dev.vicko.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.DigestOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipOutputStreamTest01 {
+	 public static void main (String[] args) {
+		  Path arquivoZip = Paths.get("pasta/arquivo.zip");
+		  Path arquivosParaZipar = Paths.get("pasta/subpasta1/ssubsubpasta1");
+		  zip(arquivoZip, arquivosParaZipar);
+	 }
+
+	 private static void zip (Path arquivoZip, Path arquivosParaZipar) {
+		  try (ZipOutputStream zipStream = new ZipOutputStream(Files.newOutputStream(arquivoZip)); DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar)) {
+				for (Path file: directoryStream){
+					 ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+					 zipStream.putNextEntry(zipEntry);
+					 Files.copy(file,zipStream);
+					 zipStream.closeEntry();
+				}
+				System.out.println("Arquivo Zipado com sucesso!");
+		  } catch (IOException e) {
+				e.printStackTrace();
+		  }
+
+	 }
+
+}
