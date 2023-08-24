624abd49138eba642a4cb74673ed47b36fe29051
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-24 20:36:44 -0300

    174 - Coleções pt 14 - Iterator

--- maratona_java/src/dev/vicko/maratonajava/javacore/Ycolections/domain/Manga.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Ycolections/domain/Manga.java
@@ -6,6 +6,7 @@ public class Manga implements Comparable<Manga> {
 	 private Long id;
 	 private String nome;
 	 private double preco;
+	 private int quantidade;
 
 	 public Manga (Long id, String nome, double preco) {
 		  Objects.requireNonNull(id, "Id não pode ser null");
@@ -13,6 +14,11 @@ public class Manga implements Comparable<Manga> {
 		  this.id = id;
 		  this.nome = nome;
 		  this.preco = preco;
+	 }
+
+	 public Manga (Long id, String nome, double preco, int quantidade) {
+		  this(id, nome,preco);
+		  this.quantidade = quantidade;
 	 }
 
 	 public Long getId () {
@@ -45,6 +51,7 @@ public class Manga implements Comparable<Manga> {
 					 "id=" + id +
 					 ", nome='" + nome + '\'' +
 					 ", preco=" + preco +
+					 ", quantidade=" + quantidade +
 					 '}';
 	 }
 
@@ -73,5 +80,13 @@ public class Manga implements Comparable<Manga> {
 		 /* return Double.compare(preco, outroManga.getPreco());
 		  return Double.valueOf(preco).compareTo(outroManga.getPreco());
 		  return this.id.compareTo(outroManga.getId());*/
+	 }
+
+	 public int getQuantidade () {
+		  return quantidade;
+	 }
+
+	 public void setQuantidade (int quantidade) {
+		  this.quantidade = quantidade;
 	 }
 }
