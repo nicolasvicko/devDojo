406638622592b37d61fe47c94af2c13b0a807324
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-24 21:38:12 -0300

    177 - Coleções pt 17 - NavigableSet, TreeSet pt 02

--- maratona_java/src/dev/vicko/maratonajava/javacore/Ycolections/test/NavigableSetTest01.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Ycolections/test/NavigableSetTest01.java
@@ -7,13 +7,22 @@ import java.util.Comparator;
 import java.util.NavigableSet;
 import java.util.TreeSet;
 
-class SmartphoneMarcaComparator implements Comparator<Smartphone>{
+class SmartphoneMarcaComparator implements Comparator<Smartphone> {
 
 	 @Override
 	 public int compare (Smartphone o1, Smartphone o2) {
 		  return o1.getMarca().compareTo(o2.getMarca());
 	 }
 }
+
+class MangaPrecoComparator implements Comparator<Manga> {
+
+	 @Override
+	 public int compare (Manga o1, Manga o2) {
+		  return Double.compare(o1.getPreco(), o2.getPreco());
+	 }
+}
+
 public class NavigableSetTest01 {
 	 public static void main (String[] args) {
 		  NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
@@ -22,7 +31,7 @@ public class NavigableSetTest01 {
 		  set.add(smartphone);
 
 
-		  NavigableSet<Manga> mangas = new TreeSet<>();
+		  NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
 		  mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
 		  mangas.add(new Manga(1L, "Beserk", 9.5, 5));
 		  mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
@@ -33,6 +42,29 @@ public class NavigableSetTest01 {
 		  for (Manga manga : mangas) {
 				System.out.println(manga);
 		  }
+
+		  Manga yuyu = new Manga(21L, "Yuyu Hakusho", 3.2, 5);
+
+		  System.out.println("---------------");
+
+		  System.out.println(mangas.lower(yuyu));
+		  System.out.println(mangas.floor(yuyu));
+		  System.out.println(mangas.higher(yuyu));
+		  System.out.println(mangas.ceiling(yuyu));
+
+		  System.out.println("---------------");
+
+		  System.out.println(mangas.size());
+		  System.out.println(mangas.pollFirst());
+		  System.out.println(mangas.pollLast());
+
+
+		  /*
+		  Lower <
+		  Floor <=
+		  Higher >
+		  Ceiling >=
+		   */
 
 	 }
 }
