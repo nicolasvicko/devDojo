4863f86fb4b08bfabf6ea7d3071bbd032042cc7a
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-24 21:09:01 -0300

    176 - Coleções pt 16 - NavigableSet, TreeSet pt 01

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/Ycolections/test/NavigableSetTest01.java
@@ -0,0 +1,38 @@
+package dev.vicko.maratonajava.javacore.Ycolections.test;
+
+import dev.vicko.maratonajava.javacore.Ycolections.domain.Manga;
+import dev.vicko.maratonajava.javacore.Ycolections.domain.Smartphone;
+
+import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+class SmartphoneMarcaComparator implements Comparator<Smartphone>{
+
+	 @Override
+	 public int compare (Smartphone o1, Smartphone o2) {
+		  return o1.getMarca().compareTo(o2.getMarca());
+	 }
+}
+public class NavigableSetTest01 {
+	 public static void main (String[] args) {
+		  NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
+
+		  Smartphone smartphone = new Smartphone("123", "Nokia");
+		  set.add(smartphone);
+
+
+		  NavigableSet<Manga> mangas = new TreeSet<>();
+		  mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
+		  mangas.add(new Manga(1L, "Beserk", 9.5, 5));
+		  mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
+		  mangas.add(new Manga(3L, "Attack on Titan", 11.20, 2));
+		  mangas.add(new Manga(2L, "Dragon Ball Z", 2.99, 0));
+		  mangas.add(new Manga(10L, "Aaragon", 2.99, 0));
+
+		  for (Manga manga : mangas) {
+				System.out.println(manga);
+		  }
+
+	 }
+}
