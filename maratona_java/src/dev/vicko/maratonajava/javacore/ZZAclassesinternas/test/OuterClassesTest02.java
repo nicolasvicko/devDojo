ba68160ad17c7eaa240d88dc1d06a334086444da
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-26 14:17:49 -0300

    190 - Classes Internas pt 02 - Classes Locais

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest02.java
@@ -0,0 +1,21 @@
+package dev.vicko.maratonajava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassesTest02 {
+	 private String nome = "Midoriya";
+
+	 void print (final String param) {
+		  final String lastName = "Izuku";
+		  class LocalClass {
+				public void printLocal () {
+					 System.out.println(nome + " "+lastName);
+				}
+		  }
+		  new LocalClass().printLocal();
+
+	 }
+
+	 public static void main (String[] args) {
+		  OuterClassesTest02 outer = new OuterClassesTest02();
+		  outer.print();
+	 }
+}
