e3793b981b0588ace641dec6d9f4c8da498b0499
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-28 16:58:04 -0300

    194 - Parametrizando comportamentos pt 02

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -0,0 +1,7 @@
+package dev.vicko.maratonajava.javacore.ZZBcomportamento.interfaces;
+
+import dev.vicko.maratonajava.javacore.ZZBcomportamento.domain.Car;
+
+public interface CarPredicate {
+	 boolean test(Car car);
+}
