9ff3fe13786b6516ccc3ca24a28a063fb6f79d69
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-30 18:23:46 -0300

    208 - Streams pt 06 - Finding and Matching

--- maratona_java/src/dev/vicko/maratonajava/javacore/ZZestreams/test/StreamTest06.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/ZZestreams/test/StreamTest06.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class StreamTest06 {
 	 private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
 				new LightNovel("Tensei Shitarra", 8.99),
-				new LightNovel("Overlord", 3.99),
+				new LightNovel("Overlord", 10.99),
 				new LightNovel("Violet Evergarden", 5.99),
 				new LightNovel("No Game no life", 2.99),
 				new LightNovel("FullMetal Alchemist", 5.99),
@@ -21,7 +21,7 @@ public class StreamTest06 {
 	 public static void main (String[] args) {
 		  System.out.println(lightNovels.stream().anyMatch(ln -> ln.getPrice() > 9));
 		  System.out.println(lightNovels.stream().allMatch(ln -> ln.getPrice() > 0));
-		  System.out.println(lightNovels.stream().noneMatch(ln -> ln.getPrice() > 0));
+		  System.out.println(lightNovels.stream().noneMatch(ln -> ln.getPrice() < 0));
 		  lightNovels.stream()
 					 .filter(lightNovel -> lightNovel.getPrice() > 3)
 					 .findAny()
