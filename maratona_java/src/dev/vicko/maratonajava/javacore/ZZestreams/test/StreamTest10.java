720f86eff465e246268d2dc3b1a63587ae60631b
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-31 12:38:54 -0300

    212 - Streams pt 10 - Gerando streams pt 02

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/ZZestreams/test/StreamTest10.java
@@ -0,0 +1,22 @@
+package dev.vicko.maratonajava.javacore.ZZestreams.test;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
+
+public class StreamTest10 {
+	 public static void main (String[] args) {
+		  Stream.iterate(1, n -> n + 2)
+					 .limit(100)
+					 .forEach(System.out::println);
+
+		  Stream.iterate(new int[] {0, 1}, n -> new int[]{n[1], n[0]+n[1]})
+					 .limit(100)
+					 .map(a->a[0])
+					 .forEach(System.out::println);
+
+		  ThreadLocalRandom random = ThreadLocalRandom.current();
+		  Stream.generate(()-> random.nextInt(1,500))
+					 .limit(90)
+					 .forEach(System.out::println);
+	 }
+}
