48f6ee6e7fc49236126c01499e037ef9b622d945
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-30 23:13:39 -0300

    210 - Streams pt 08 - Reduce pt 02

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/ZZestreams/test/StreamTest08.java
@@ -0,0 +1,36 @@
+package dev.vicko.maratonajava.javacore.ZZestreams.test;
+
+import dev.vicko.maratonajava.javacore.ZZestreams.domain.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamTest08 {
+	 private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+				new LightNovel("Tensei Shitarra", 8.99),
+				new LightNovel("Overlord", 10.99),
+				new LightNovel("Violet Evergarden", 5.99),
+				new LightNovel("No Game no life", 2.99),
+				new LightNovel("FullMetal Alchemist", 5.99),
+				new LightNovel("Kumo desuga", 1.99),
+				new LightNovel("Kumo desuga", 1.99),
+				new LightNovel("Monotogatari", 4.00)
+	 ));
+
+	 public static void main (String[] args) {
+
+		  lightNovels.stream()
+					 .map(LightNovel::getPrice)
+					 .filter(price -> price > 3)
+					 .reduce(Double::sum)
+					 .ifPresent(System.out::println);
+
+		  double sum = lightNovels.stream()
+					 .mapToDouble(LightNovel::getPrice)
+					 .filter(price -> price > 3)
+					 .sum();
+
+
+
+	 }
+}
