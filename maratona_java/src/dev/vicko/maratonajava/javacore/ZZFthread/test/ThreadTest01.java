73d8c5f549279696ab11f64a609844c929b2dae1
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-08-31 17:48:40 -0300

    221 - Threads pt 02 - Estados das threads

--- maratona_java/src/dev/vicko/maratonajava/javacore/ZZFthread/test/ThreadTest01.java
+++ maratona_java/src/dev/vicko/maratonajava/javacore/ZZFthread/test/ThreadTest01.java
@@ -54,6 +54,8 @@ public class ThreadTest01 {
 		  t2.start();
 		  t3.start();
 		  t4.start();
+		  System.out.println("###################################"+Thread.currentThread().getName());
+
 
 
 
