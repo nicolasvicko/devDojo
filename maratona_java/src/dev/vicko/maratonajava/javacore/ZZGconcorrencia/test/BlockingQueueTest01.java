846bfeb0616923f06230bdf108e1379f83d22384
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2023-09-04 11:22:22 -0300

    255 - JDBC pt 04 - Instalando e adicionando maven ao projeto

---
+++ maratona_java/src/dev/vicko/maratonajava/javacore/ZZGconcorrencia/test/BlockingQueueTest01.java
@@ -0,0 +1,32 @@
+package dev.vicko.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import static java.lang.System.out;
+
+public class BlockingQueueTest01 {
+	 public static void main (String[] args) throws InterruptedException {
+		  BlockingQueue<String> bq = new ArrayBlockingQueue<>(1);
+		  bq.put("William");
+		  out.printf("%s added the value %s%n", Thread.currentThread().getName(), bq.peek());
+		  out.println("Trying to add another value");
+		  bq.put("Suane");
+		  out.printf("%s added the value %s%n", Thread.currentThread().getName(), bq.peek());
+
+	 }
+
+	 static class RemoveFromQueue implements Runnable{
+		  private final BlockingQueue<String> bq;
+
+		  RemoveFromQueue (BlockingQueue<String> bq) {
+				this.bq = bq;
+		  }
+
+		  @Override
+		  public void run () {
+				out.printf("%s added the value %s%n", Thread.currentThread().getName(), bq.peek());
+
+		  }
+	 }
+}
